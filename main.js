f540443d8cde0142af829d05c1c5af6bd241d96d
YashAgarwal44 <77672385+YashAgarwal44@users.noreply.github.com>
2022-06-13 16:00:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-music_1 = 
-music_2 = 
+music_1 = "music.mp3";
+music_2 = "music2.mp3";
 leftWristX = 0;
 leftWristY = 0;
+scoreLeftWrist = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreRightWrist = 0;
+song_1_status = "";
+song_2_status = "";
 
 function preload(){
-    song_1 = loadSound("");
-    song_2 = loadSound("");
-}
-
-function draw(){
-    image(video, 0, 0, 600, 500);
+    music_1 = loadSound("music.mp3");
+    music_2 = loadSound("music2.mp3");
 }
 
 function setup(){
@@ -23,6 +25,23 @@ function setup(){
     poseNet.on('pose', gotPoses);
 }
 
+function draw(){
+    image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000")
+
+    if(scoreLeftWrist > 0.2){
+
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+        
+}
+} 
+
 function modelLoaded(){
     console.log("Posenet Is Initialized");
 }
@@ -30,6 +49,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
