b6027f5467657aff2e2ed589361963f90b311352
YashAgarwal44 <77672385+YashAgarwal44@users.noreply.github.com>
2022-06-15 20:02:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,23 +25,6 @@ function setup(){
     poseNet.on('pose', gotPoses);
 }
 
-function draw(){
-    image(video, 0, 0, 600, 500);
-    fill("#FF0000");
-    stroke("#FF0000")
-
-    if(scoreLeftWrist > 0.2){
-
-        circle(leftWristX, leftWristY, 20);
-        InNumberleftWristY = Number(leftWristY);
-        remove_decimals = floor(InNumberleftWristY);
-        volume = remove_decimals/500;
-        document.getElementById("volume").innerHTML = "Volume = " + volume;
-        song.setVolume(volume);
-        
-}
-} 
-
 function modelLoaded(){
     console.log("Posenet Is Initialized");
 }
@@ -50,10 +33,50 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist = " + scoreRightWrist +"scoreLeftWrist = " + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
     }
+}
+
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+    song_1_status = music_1.isPlaying();
+    song_2_status = music_2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX, rightWristY, 20);
+        music_2.stop();
+        if(song_1_status == false){
+            music_1.play();
+            document.getElementById("song").innerHTML = "Playing Song - Harry Potter";
+        }
+    }
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+        music_1.stop();
+        if(song_2_status == false){
+            music_2.play();
+            document.getElementById("song").innerHTML = "Playing Song - Peter Pan";
+        }
+    }
+}
+
+function play(){
+    music_1.play();
+    music_1.setVolume(1);
+    music_1.rate(1);
 }
